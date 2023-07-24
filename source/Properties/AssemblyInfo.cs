ba0c4fc2022cf036e0c15b1ca4aa51188bbca3d3
Jaxe <jaxe.wilde@gmail.com>
2023-07-25 07:03:51 +0800

    Test Commit

---
+++ source/Properties/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System.Reflection;
+using CodeExplorer;
+
+[assembly: AssemblyTitle(Mod.Name)]
+[assembly: AssemblyProduct("RimWorld Mods by Jaxe")]
+[assembly: AssemblyCopyright("© Jaxe")]
+[assembly: AssemblyVersion(Mod.Version)]
