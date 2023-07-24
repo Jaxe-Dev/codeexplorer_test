d7f6ce28fb60e3d2ef3d444b87f07f0b9b238584
Jaxe <42095078+Jaxe-Dev@users.noreply.github.com>
2023-07-25 07:03:51 +0800

    Test Commit

---
+++ source/Patch/RimWorld_PlaySettings_DoPlaySettingsGlobalControls.cs
@@ -0,0 +1,37 @@
+﻿using CodeExplorer.Core;
+using HarmonyLib;
+using Verse;
+
+namespace CodeExplorer.Patch
+{
+  [HarmonyPatch(typeof(DebugWindowsOpener), "DrawButtons")]
+  public static class RimWorld_PlaySettings_DoPlaySettingsGlobalControls
+  {
+    private static void Postfix(WidgetRow ___widgetRow)
+    {
+      if (!___widgetRow.ButtonIcon(Textures.ToggleIcon, Mod.Name)) { return; }
+
+      Dialog_Main.Open();
+    }
+  }
+}
+// using CodeExplorer.Core;
+// using HarmonyLib;
+// using RimWorld;
+// using Verse;
+//
+// namespace CodeExplorer.Patch
+// {
+//   [HarmonyPatch(typeof(PlaySettings), nameof(PlaySettings.DoPlaySettingsGlobalControls))]
+//   public static class RimWorld_PlaySettings_DoPlaySettingsGlobalControls
+//   {
+//     private static void Postfix(WidgetRow row, bool worldView)
+//     {
+//       if (worldView || row == null) { return; }
+//
+//       if (!row.ButtonIcon(Textures.ToggleIcon, Mod.Name)) { return; }
+//
+//       Dialog_Main.Open();
+//     }
+//   }
+// }
